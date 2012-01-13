b2b070d88fd8a0c35ccfeda56416b880da67f80f
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-01-13 01:08:11 -0500

    Basic GameConfig class, used to store, errr, the game's configuration.

---
+++ src/cc/co/majteam/redemption/game/GameConfig.java
@@ -0,0 +1,45 @@
+package cc.co.majteam.redemption.game;
+
+public class GameConfig {
+	private static GameConfig instance = new GameConfig();
+	
+	public static GameConfig getInstance() {
+		return instance;
+	}
+	
+	private int height, width;
+	
+	private GameConfig() {
+	}
+	
+	public boolean load() {
+		// TODO: Remove hard-coded values and load from ConfigFile instead.
+		this.width = 800;
+		this.height = 600;
+		
+		return true;
+	}
+	
+	public boolean save() {
+		// TODO: Make it actually save, using ConfigFile
+		return true;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	
+}
