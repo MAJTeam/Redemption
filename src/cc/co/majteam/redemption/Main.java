8d3ee1834c2bef484513b8d445261dbe63dcafc3
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-01-13 01:21:49 -0500

    Simple Main class to start things off.

---
+++ src/cc/co/majteam/redemption/Main.java
@@ -0,0 +1,15 @@
+package cc.co.majteam.redemption;
+
+import cc.co.majteam.redemption.handlers.GameHandler;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GameHandler.getInstance().start();
+
+	}
+
+}
