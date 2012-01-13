acbb68ad5bf31fa031aecb5b1629f5dbafb77943
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-01-13 00:17:29 -0500

    Added Player

---
+++ src/cc/co/majteam/redemption/handlers/player/Player.java
@@ -0,0 +1,27 @@
+package cc.co.majteam.redemption.handlers.player;
+
+public class Player {
+	private String name;
+	private int score;
+	
+	public Player(String name) {
+		this.name = name;
+		this.score = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
