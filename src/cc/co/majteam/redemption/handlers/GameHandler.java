ee429e10c35b8004e27146ab60df7eaf05dcdb11
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-01-13 01:15:25 -0500

    Linked GameHandler with GameState and GameConfig.
    We're slowly getting somewhere now. (^_^)

--- src/cc/co/majteam/redemption/handlers/GameHandler.java
+++ src/cc/co/majteam/redemption/handlers/GameHandler.java
@@ -1,5 +1,9 @@
 package cc.co.majteam.redemption.handlers;
 
+import cc.co.majteam.redemption.game.GameConfig;
+import cc.co.majteam.redemption.game.GameState;
+
+
 public class GameHandler {
 	private static GameHandler instance = new GameHandler();
 	
@@ -7,6 +11,14 @@ public class GameHandler {
 		return instance;
 	}
 	
+	private GameState gameState;
+	private GameConfig gameConfig;
+	
 	private GameHandler() {
+		this.gameState = GameState.getInstance();
+		this.gameConfig = GameConfig.getInstance();
+	}
+	
+	public void start() {
 	}
 }
