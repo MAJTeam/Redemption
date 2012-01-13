207db2f5a6fc127b89b2876ac5ba22fdf1f635e0
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-01-13 01:39:02 -0500

    Fleshed out start() in GameHandler, we now have a GraphicWindow! =D

--- src/cc/co/majteam/redemption/handlers/GameHandler.java
+++ src/cc/co/majteam/redemption/handlers/GameHandler.java
@@ -1,24 +1,31 @@
 package cc.co.majteam.redemption.handlers;
 
+import roujo.lib.gui.windows.GraphicWindow;
 import cc.co.majteam.redemption.game.GameConfig;
 import cc.co.majteam.redemption.game.GameState;
 
-
 public class GameHandler {
 	private static GameHandler instance = new GameHandler();
-	
+
 	public static GameHandler getInstance() {
 		return instance;
 	}
-	
+
 	private GameState gameState;
 	private GameConfig gameConfig;
-	
+	private GraphicWindow gameWindow;
+
 	private GameHandler() {
 		this.gameState = GameState.getInstance();
 		this.gameConfig = GameConfig.getInstance();
 	}
-	
+
 	public void start() {
+		gameConfig.load();
+		gameState.init();
+		gameWindow = new GraphicWindow("Redemption", gameConfig.getWidth(),
+				gameConfig.getHeight());
+		gameWindow.setVisible(true);
+		gameWindow.draw();
 	}
 }
