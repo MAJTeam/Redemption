7e479c6b4805d98fcf17f3d18305d649a6774e98
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-06-01 22:33:39 -0400

    Removed Cicle.center. Having that and Drawable.offset is redundant.

--- src/cc/co/majteam/redemption/shapes/Circle.java
+++ src/cc/co/majteam/redemption/shapes/Circle.java
@@ -1,24 +1,10 @@
 package cc.co.majteam.redemption.shapes;
 
 public class Circle implements Shape {
-	private Coords center;
 	private int radius;
-
-	public Circle(int x, int y, int radius) {
-		this(new Coords(x, y), radius);
-	}
 	
-	public Circle(Coords center, int radius) {
-		this.center = center;
+	public Circle(int radius) {
 		this.radius = radius;
-	}
-
-	public Coords getCenter() {
-		return center;
-	}
-
-	public void setCenter(Coords center) {
-		this.center = center;
 	}
 
 	public int getRadius() {
