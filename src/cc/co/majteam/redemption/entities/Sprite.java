485b6bf62576c655f4cc7f1fc9a1a103b0bb65a4
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-06-01 22:27:54 -0400

    Removed Color attribute for Sprite, since Drawables will have it.

--- src/cc/co/majteam/redemption/entities/Sprite.java
+++ src/cc/co/majteam/redemption/entities/Sprite.java
@@ -1,22 +1,19 @@
 package cc.co.majteam.redemption.entities;
 
-import java.awt.Color;
 import java.util.Set;
 
 import cc.co.majteam.redemption.shapes.Coords;
 import cc.co.majteam.redemption.shapes.Drawable;
 
-public abstract class Sprite {	
+public abstract class Sprite {
 	private Coords center;
-	private Color color;
 	private Set<Drawable> drawables;
-	
-	public Sprite(Color color, int x, int y) {
-		this(color, new Coords(x, y));
+
+	public Sprite(int x, int y) {
+		this(new Coords(x, y));
 	}
-	
-	public Sprite(Color color, Coords center) {
-		this.color = color;
+
+	public Sprite(Coords center) {
 		this.setCenter(center);
 	}
 
@@ -28,22 +25,14 @@ public abstract class Sprite {
 		this.center = center;
 	}
 
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
 	public void addDrawable(Drawable primitive) {
 		drawables.add(primitive);
 	}
-	
+
 	public void removeDrawable(Drawable primitive) {
 		drawables.remove(primitive);
 	}
-	
+
 	public Set<Drawable> getDrawables() {
 		return drawables;
 	}
