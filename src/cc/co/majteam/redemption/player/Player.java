d6fb80b2df38f8618291f48bdd315daf8f9b04ac
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-07-07 20:35:15 -0400

    Added Coords and Speed to Player.

--- src/cc/co/majteam/redemption/player/Player.java
+++ src/cc/co/majteam/redemption/player/Player.java
@@ -2,14 +2,18 @@ package cc.co.majteam.redemption.player;
 
 import java.awt.Color;
 
+import cc.co.majteam.redemption.graphics.Coords;
 import cc.co.majteam.redemption.graphics.sprites.PlayerSprite;
 import cc.co.majteam.redemption.player.input.KeyConfig;
 
 public class Player {
+	public static final int DefaultPlayerSpeed = 5;
+	
 	private int id;
 	private String name;
 	private Color color;
 	private int score;
+	private int speed;
 	private PlayerSprite sprite;
 	private KeyConfig keyConfig;
 	
@@ -18,6 +22,7 @@ public class Player {
 		this.name = defaults.getName();
 		this.color = defaults.getColor();
 		this.score = 0;
+		this.speed = DefaultPlayerSpeed;
 		this.sprite = new PlayerSprite(defaults);
 		this.keyConfig = defaults.getKeyDefaults().getKeyConfig();
 	}
@@ -60,5 +65,21 @@ public class Player {
 	
 	public KeyConfig getKeyConfig() {
 		return keyConfig;
+	}
+	
+	public Coords getCoords() {
+		return sprite.getCenter();
+	}
+	
+	public void setCoords(Coords coords) {
+		sprite.setCenter(coords);
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
 	}
 }
