1f78954d72dff72b1dcc14cf1d0f13bc93718793
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-07-14 17:44:48 -0400

    Added Coords.mindpoint() static method.

--- src/cc/co/majteam/redemption/graphics/Coords.java
+++ src/cc/co/majteam/redemption/graphics/Coords.java
@@ -3,7 +3,14 @@ package cc.co.majteam.redemption.graphics;
 import cc.co.majteam.redemption.graphics.sprites.Direction;
 
 public class Coords {
+	public static Coords midpoint(Coords p1, Coords p2) {
+		return midpoint((int) p1.x, (int) p1.y, (int) p2.x, (int) p2.y);
+	}
 	
+	public static Coords midpoint(int x1, int y1, int x2, int y2) {
+		return new Coords((x2 - x1) / 2 + x1, (y2 - y1) / 2 + y1);
+	}
+
 	private double x, y;
 
 	public Coords(double x, double y) {
@@ -31,15 +38,15 @@ public class Coords {
 	public void setY(double y) {
 		this.y = y;
 	}
-	
+
 	public boolean isBetween(Coords p1, Coords p2) {
 		return isBetween(p1.x, p1.y, p2.x, p2.y);
 	}
-	
+
 	public boolean isBetween(double x1, double y1, double x2, double y2) {
 		return x > x1 && y > y1 && x < x2 && y < y2;
 	}
-	
+
 	public void move(Direction direction, int dist) {
 		switch (direction) {
 		case South:
@@ -55,27 +62,27 @@ public class Coords {
 			y -= dist;
 			break;
 		case NorthEast:
-			x += dist/2 * Math.sqrt(2);
-			y -= dist/2 * Math.sqrt(2);
+			x += dist / 2 * Math.sqrt(2);
+			y -= dist / 2 * Math.sqrt(2);
 			break;
 		case NorthWest:
-			x -= dist/2 * Math.sqrt(2);
-			y -= dist/2 * Math.sqrt(2);
+			x -= dist / 2 * Math.sqrt(2);
+			y -= dist / 2 * Math.sqrt(2);
 			break;
 		case SouthEast:
-			x += dist/2 * Math.sqrt(2);
-			y += dist/2 * Math.sqrt(2);
+			x += dist / 2 * Math.sqrt(2);
+			y += dist / 2 * Math.sqrt(2);
 			break;
 		case SouthWest:
-			x -= dist/2 * Math.sqrt(2);
-			y += dist/2 * Math.sqrt(2);
+			x -= dist / 2 * Math.sqrt(2);
+			y += dist / 2 * Math.sqrt(2);
 			break;
 		default:
 			System.out.println("Invalid Direction @ Coords.move()");
 			break;
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		return "(" + x + " , " + y + ")";
